9f85720aa358cf825914e432a95f498f397b41fd
Mustafa Fırat <mustafam20503@gmail.com>
2025-09-02 19:37:57 +0300

    number of customer per floor

--- restaurant.go
+++ restaurant.go
@@ -16,9 +16,12 @@ func main() {
 		Addr: "localhost:6379",
 	})
 
-	countStr, err := client.Get(ctx, "currentCount").Result()
-	if err != nil || countStr == "" {
-		client.Set(ctx, "currentCount", 0, 0)
+	for i := 1; i <= 3; i++ {
+		key := "floor:" + strconv.Itoa(i)
+		_, err := client.Get(ctx, key).Result()
+		if err == redis.Nil {
+			client.Set(ctx, key, 0, 0)
+		}
 	}
 
 	app := fiber.New()
@@ -26,6 +29,7 @@ func main() {
 
 		EntranceStr := c.Query("Entrance", "0")
 		ExitStr := c.Query("Exit", "0")
+		FloorStr := c.Query("Floor", "0")
 
 		Entrance, err := strconv.Atoi(EntranceStr)
 		if err != nil {
@@ -35,10 +39,16 @@ func main() {
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Invalid value for Exit"})
 		}
+		Floor, err := strconv.Atoi(FloorStr)
+		if err != nil || Floor < 1 || 3 < Floor {
+			return c.JSON(fiber.Map{"error": "Invalid value for Floor"})
+
+		}
+		key := "floor:" + strconv.Itoa(Floor)
 
 		var change = Entrance - Exit
 
-		currentStr, err := client.Get(ctx, "currentCount").Result()
+		currentStr, err := client.Get(ctx, key).Result()
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Read error for Redis"})
 		}
@@ -50,29 +60,48 @@ func main() {
 		if current < 0 {
 			current = 0
 		}
-		err = client.Set(ctx, "currentCount", current, 0).Err()
+		err = client.Set(ctx, key, current, 0).Err()
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Write  error for Redis"})
 		}
 		return c.JSON(fiber.Map{
-			"message ": "Customer nums updated", "current ": change,
+			"message ": "Customer nums updated",
+			"floor":    Floor,
+			"current ": change,
 		})
 	})
 
 	app.Get("/count", func(c *fiber.Ctx) error {
-		countStr, err := client.Get(ctx, "currentCount").Result()
+		countStr1, err := client.Get(ctx, "floor:1").Result()
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Read error for Redis"})
 		}
-		count, err := strconv.Atoi(countStr)
+		count1, err := strconv.Atoi(countStr1)
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Value conversation error for Redis"})
 		}
-
+		countStr2, err := client.Get(ctx, "floor:2").Result()
+		if err != nil {
+			return c.JSON(fiber.Map{"error": "Read error for Redis"})
+		}
+		count2, err := strconv.Atoi(countStr2)
+		if err != nil {
+			return c.JSON(fiber.Map{"error": "Value conversation error for Redis"})
+		}
+		countStr3, err := client.Get(ctx, "floor:3").Result()
+		if err != nil {
+			return c.JSON(fiber.Map{"error": "Read error for Redis"})
+		}
+		count3, err := strconv.Atoi(countStr3)
+		if err != nil {
+			return c.JSON(fiber.Map{"error": "Value conversation error for Redis"})
+		}
 		return c.JSON(fiber.Map{
-			"Count": count,
+			"floor1": count1,
+			"floor2": count2,
+			"floor3": count3,
+			"total":  count1 + count2 + count3,
 		})
-
 	})
 	app.Listen(":8080")
 
