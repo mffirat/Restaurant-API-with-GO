45d81b4b47fb10896e3cf2f52991635bbca52062
Mustafa Fırat <mustafam20503@gmail.com>
2025-09-24 20:59:14 +0300

    connected to postgres and added report handlers

--- update.go
+++ update.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func UpdateHandler(c *fiber.Ctx) error {
 
-	EntranceStr := c.Query("Entrance", "0")
-	ExitStr := c.Query("Exit", "0")
+	action := c.Query("action", "enter")
 	FloorStr := c.Query("Floor", "1")
-
-	Entrance, err := strconv.Atoi(EntranceStr)
-	if err != nil {
-		return c.JSON(fiber.Map{"error": "Invalid value for Entrance: " + err.Error()})
-	}
-	Exit, err := strconv.Atoi(ExitStr)
-	if err != nil {
-		return c.JSON(fiber.Map{"error": "Invalid value for Exit: " + err.Error()})
-	}
+	Gender := c.Query("Gender", "unknown")
+	AgeGroup := c.Query("AgeGroup", "adult")
+	idStr := c.Query("id", "0")
+	paymentStr := c.Query("Payment", "0")
 	Floor, err := strconv.Atoi(FloorStr)
 	if err != nil || Floor < 1 || 3 < Floor {
 		return c.JSON(fiber.Map{"error": "Invalid value for Floor: " + err.Error()})
@@ -27,24 +22,63 @@ func UpdateHandler(c *fiber.Ctx) error {
 	}
 	key := "floor:" + strconv.Itoa(Floor)
 
-	var change = Entrance - Exit
+	if action == "enter" {
 
-	if change > 0 {
-		_, err = client.IncrBy(ctx, key, int64(change)).Result()
+		customer := Customer{
+			Gender:    Gender,
+			AgeGroup:  AgeGroup,
+			Floor:     Floor,
+			Payment:   0,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			ExitedAt:  nil,
+		}
+		DB.Create(&customer)
+
+		_, err := client.IncrBy(ctx, key, 1).Result()
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Redis INCRBY error: " + err.Error()})
 		}
-	} else if change < 0 {
-		_, err = client.DecrBy(ctx, key, int64(-change)).Result()
+
+		return c.JSON(fiber.Map{
+			"message": "Customer entered",
+			"id":      customer.ID,
+			"floor":   Floor,
+		})
+	} else if action == "exit" {
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil || id <= 0 {
+			return c.JSON(fiber.Map{"error": "Invalid ID"})
+		}
+		payment, err := strconv.ParseFloat(paymentStr, 64)
+		if err != nil || id <= 0 {
+			return c.JSON(fiber.Map{"error": "Invalid Payment"})
+		}
+
+		var customer Customer
+		result := DB.First(&customer, id)
+		if result.Error != nil {
+			return c.JSON(fiber.Map{"error": "Customer not found"})
+		}
+
+		now := time.Now()
+		customer.Payment = payment
+		customer.ExitedAt = &now
+		DB.Save(&customer)
+
+		_, err = client.DecrBy(ctx, key, 1).Result()
 		if err != nil {
 			return c.JSON(fiber.Map{"error": "Redis DECRBY error: " + err.Error()})
 		}
-	}
-	response := UpdateResponse{
-		Message: "Customer nums updated",
-		Floor:   Floor,
-		Current: change,
+
+		return c.JSON(fiber.Map{
+			"message":  "Customer exited",
+			"id":       id,
+			"payment":  payment,
+			"exitedAt": now,
+		})
 	}
 
-	return c.JSON(response)
+	return c.JSON(fiber.Map{"error": "Invalid action"})
 }
